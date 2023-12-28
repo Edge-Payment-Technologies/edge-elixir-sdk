cef7a54f5dc8f4a08e7a3ac6ce8ba843c28272cf
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2023-12-28 00:26:58 -0800

    Getting smarter about handling query parameters

--- lib/edge_payment_client.ex
+++ lib/edge_payment_client.ex
@@ -1,5 +1,13 @@
 defmodule EdgePaymentClient do
-  @enforce_keys [:authorization, :user_agent, :host, :json_decoder, :json_encoder, :namespace, :finch_options]
+  @enforce_keys [
+    :authorization,
+    :user_agent,
+    :host,
+    :json_decoder,
+    :json_encoder,
+    :namespace,
+    :finch_options
+  ]
   defstruct authorization: nil,
             user_agent: nil,
             host: "api.tryedge.com",
@@ -7,6 +15,14 @@ defmodule EdgePaymentClient do
             json_encoder: &Jason.encode/1,
             namespace: EdgePaymentClient.Finch,
             finch_options: []
+
+  @default_query_options %{
+    filters: %{},
+    include: [],
+    sort: [],
+    page: [],
+    fields: %{}
+  }
 
   @type raw() :: %{
           token: String.t(),
@@ -30,18 +46,23 @@ defmodule EdgePaymentClient do
 
   @spec client(raw()) :: t()
   def client(%{token: token, user_agent: user_agent} = properties) when is_map(properties) do
-    struct(__MODULE__, %{
-      authorization: "Bearer #{token}",
-      user_agent: user_agent
-    } |> Map.merge(properties))
+    struct(
+      __MODULE__,
+      %{
+        authorization: "Bearer #{token}",
+        user_agent: user_agent
+      }
+      |> Map.merge(properties)
+    )
   end
 
-  @spec get(EdgePaymentClient.t(), String.t(), map()) :: {:ok, any()} | {:error, any()}
-  def get(client, path, data)
-      when is_struct(client, EdgePaymentClient) and is_binary(path) and is_map(data) do
+  @spec get(EdgePaymentClient.t(), String.t(), Keyword.t() | nil) ::
+          {:ok, any()} | {:error, any()}
+  def get(client, path, query \\ [])
+      when is_struct(client, EdgePaymentClient) and is_binary(path) do
     Finch.build(
       :get,
-      encode_with_query(client.host, path, data),
+      encode_uri(client.host, path, with_query_defaults(query)),
       default_headers(client, [
         {"Accept", "application/vnd.api+json"}
       ])
@@ -50,30 +71,33 @@ defmodule EdgePaymentClient do
     |> response(client)
   end
 
-  @spec options(EdgePaymentClient.t(), String.t(), map()) :: {:ok, any()}
-  def options(client, path, data)
-      when is_struct(client, EdgePaymentClient) and is_binary(path) and is_map(data) do
+  @spec options(EdgePaymentClient.t(), String.t(), Keyword.t() | nil) :: {:ok, any()}
+  def options(client, path, query \\ [])
+      when is_struct(client, EdgePaymentClient) and is_binary(path) do
     Finch.build(
       :options,
-      encode_with_query(client.host, path, data),
+      encode_uri(client.host, path, with_query_defaults(query)),
       default_headers(client)
     )
     |> request(client)
+    |> response(client)
   end
 
-  @spec delete(EdgePaymentClient.t(), String.t(), map()) :: {:ok, any()}
-  def delete(client, path, data)
-      when is_struct(client, EdgePaymentClient) and is_binary(path) and is_map(data) do
+  @spec delete(EdgePaymentClient.t(), String.t(), Keyword.t() | nil) :: {:ok, any()}
+  def delete(client, path, query \\ [])
+      when is_struct(client, EdgePaymentClient) and is_binary(path) do
     Finch.build(
       :delete,
-      encode_with_query(client.host, path, data),
+      encode_uri(client.host, path, with_query_defaults(query)),
       default_headers(client)
     )
     |> request(client)
+    |> response(client)
   end
 
-  @spec post(EdgePaymentClient.t(), String.t(), map()) :: {:ok, any()} | {:error}
-  def post(client, path, data)
+  @spec post(EdgePaymentClient.t(), String.t(), map(), Keyword.t() | nil) ::
+          {:ok, any()} | {:error}
+  def post(client, path, data, query \\ [])
       when is_struct(client, EdgePaymentClient) and is_binary(path) and is_map(data) do
     data
     |> client.json_encoder.()
@@ -81,7 +105,7 @@ defmodule EdgePaymentClient do
       {:ok, payload} ->
         Finch.build(
           :post,
-          encode_without_query(client.host, path),
+          encode_uri(client.host, path, with_query_defaults(query)),
           default_headers(client, [
             {"Content-Type", "application/vnd.api+json"},
             {"Accept", "application/vnd.api+json"}
@@ -89,22 +113,15 @@ defmodule EdgePaymentClient do
           payload
         )
         |> request(client)
-        |> case do
-          {:ok, response} ->
-            response.body
-            |> client.json_decoder.()
-
-          error ->
-            error
-        end
+        |> response(client)
 
       error ->
         error
     end
   end
 
-  @spec patch(EdgePaymentClient.t(), String.t(), map()) :: {:ok, any()}
-  def patch(client, path, data)
+  @spec patch(EdgePaymentClient.t(), String.t(), map(), Keyword.t() | nil) :: {:ok, any()}
+  def patch(client, path, data, query \\ [])
       when is_struct(client, EdgePaymentClient) and is_binary(path) and is_map(data) do
     data
     |> client.json_encoder.()
@@ -112,7 +129,7 @@ defmodule EdgePaymentClient do
       {:ok, payload} ->
         Finch.build(
           :patch,
-          encode_without_query(client.host, path),
+          encode_uri(client.host, path, with_query_defaults(query)),
           default_headers(client, [
             {"Content-Type", "application/vnd.api+json"},
             {"Accept", "application/vnd.api+json"}
@@ -120,22 +137,15 @@ defmodule EdgePaymentClient do
           payload
         )
         |> request(client)
-        |> case do
-          {:ok, response} ->
-            response.body
-            |> client.json_decoder.()
-
-          error ->
-            error
-        end
+        |> response(client)
 
       error ->
         error
     end
   end
 
-  @spec put(EdgePaymentClient.t(), String.t(), map()) :: {:ok, any()}
-  def put(client, path, data)
+  @spec put(EdgePaymentClient.t(), String.t(), map(), Keyword.t() | nil) :: {:ok, any()}
+  def put(client, path, data, query \\ [])
       when is_struct(client, EdgePaymentClient) and is_binary(path) and is_map(data) do
     data
     |> client.json_encoder.()
@@ -143,7 +153,7 @@ defmodule EdgePaymentClient do
       {:ok, payload} ->
         Finch.build(
           :put,
-          encode_without_query(client.host, path),
+          encode_uri(client.host, path, with_query_defaults(query)),
           default_headers(client, [
             {"Content-Type", "application/vnd.api+json"},
             {"Accept", "application/vnd.api+json"}
@@ -151,38 +161,36 @@ defmodule EdgePaymentClient do
           payload
         )
         |> request(client)
-        |> case do
-          {:ok, response} ->
-            response.body
-            |> client.json_decoder.()
-
-          error ->
-            error
-        end
+        |> response(client)
 
       error ->
         error
     end
   end
 
-  defp encode_without_query(host, path)
+  defp encode_uri(host, path, nil)
        when is_binary(host) and is_binary(path),
-       do: encode_without_query(URI.parse("https://#{host}"), URI.parse(path))
+       do:
+         encode_uri(
+           URI.parse("https://#{host}"),
+           URI.parse(path),
+           nil
+         )
 
-  defp encode_without_query(host, path)
+  defp encode_uri(host, path, nil)
        when is_struct(host, URI) and is_struct(path, URI),
-       do: URI.merge(host, path)
+       do: URI.parse("#{host}#{path}")
 
-  defp encode_with_query(host, path, query)
+  defp encode_uri(host, path, query)
        when is_binary(host) and is_binary(path) and is_map(query),
        do:
-         encode_with_query(
+         encode_uri(
            URI.parse("https://#{host}"),
            URI.parse(path),
            Plug.Conn.Query.encode(query)
          )
 
-  defp encode_with_query(host, path, query)
+  defp encode_uri(host, path, query)
        when is_struct(host, URI) and is_struct(path, URI) and is_binary(query),
        do: URI.parse("#{host}#{path}?#{query}")
 
@@ -236,5 +244,36 @@ defmodule EdgePaymentClient do
 
   defp response({:error, error}, _edge_client) do
     {:error, error}
+  end
+
+  defp with_query_defaults(nil), do: nil
+  defp with_query_defaults([]), do: nil
+
+  defp with_query_defaults(query) when is_list(query) do
+    %{
+      filter: query |> Keyword.get(:filter, @default_query_options[:filter]),
+      fields:
+        query
+        |> Keyword.get(:fields, @default_query_options[:fields])
+        |> encode_fields_query(),
+      sort: query |> Keyword.get(:sort, @default_query_options[:sort]) |> Enum.join(","),
+      page: query |> Keyword.get(:page, @default_query_options[:page]),
+      include: query |> Keyword.get(:include, @default_query_options[:include]) |> Enum.join(",")
+    }
+    |> Enum.filter(fn
+      {_key, %{}} -> false
+      {_key, []} -> false
+      {_key, ""} -> false
+      {_key, nil} -> false
+      {_key, _value} -> true
+    end)
+    |> Map.new()
+  end
+
+  defp with_query_defaults(%{}), do: %{}
+  defp encode_fields_query(fields) when is_map(fields) do
+    fields
+    |> Enum.map(fn {resource, attributes} when is_list(attributes) -> {resource, Enum.join(attributes, ",")} end)
+    |> Map.new()
   end
 end
--- lib/edge_payment_client/customer.ex
+++ lib/edge_payment_client/customer.ex
@@ -1,59 +1,73 @@
 defmodule EdgePaymentClient.Customer do
-  @enforce_keys [:id, :created_at, :updated_at, :__record__]
-  defstruct id: nil, created_at: nil, updated_at: nil, __record__: nil
+  @enforce_keys [:id, :name, :created_at, :updated_at, :__record__]
+  defstruct id: nil, name: nil, created_at: nil, updated_at: nil, __record__: nil
 
   @path "/customers"
+  @resource_type "customers"
 
   @type t() :: %__MODULE__{
           id: String.t(),
+          name: String.t(),
           #  TODO: Change to date time
           created_at: String.t(),
           updated_at: String.t(),
           __record__: map()
         }
 
-  @spec list(EdgePaymentClient.t(), map(), list(), list()) :: t() | nil
-  def list(client, filters \\ %{}, include \\ [], fields \\ [], sort \\ [])
+  @spec list(EdgePaymentClient.t(), Keyword.t() | nil) :: list(t()) | {:error, any()}
+  def list(client, query \\ [])
       when is_struct(client, EdgePaymentClient) do
     client
-    |> EdgePaymentClient.get("#{@path}", %{
-      "filter" => filters,
-      "fields" => Enum.join(fields, ","),
-      "include" => Enum.join(include, ","),
-      "sort" => Enum.join(sort, ",")
-    })
+    |> EdgePaymentClient.get("#{@path}", query)
     |> case do
       {:ok,
        %{
          json: %{
-            "data" => records
-          }
+           "data" => entities
+         }
        }} ->
-        Enum.map(records, fn %{
-            "id" => id,
-            "attributes" => %{"created_at" => created_at, "updated_at" => updated_at}
-          } = record ->
-          %__MODULE__{
-            id: id,
+        Enum.map(entities, &struct_from_entity/1)
 
-            # TODO: Parse date time
-            created_at: created_at,
-            updated_at: updated_at,
-            __record__: record
-          }
-        end)
-
-      error -> error
+      error ->
+        error
     end
   end
 
-  @spec show(EdgePaymentClient.t(), String.t()) :: nil
+  @spec show(EdgePaymentClient.t(), String.t()) :: t() | nil | {:error, any()}
   def show(client, id) when is_struct(client, EdgePaymentClient) and is_binary(id) do
+    client
+    |> EdgePaymentClient.get("#{@path}/#{id}")
+    |> case do
+      {:ok,
+       %{
+         json: %{
+           "data" => entity
+         }
+       }} ->
+        struct_from_entity(entity)
+
+      error ->
+        error
+    end
   end
 
-  @spec create(EdgePaymentClient.t(), map) :: nil
+  @spec create(EdgePaymentClient.t(), map) :: t() | {:error, any()}
   def create(client, attributes)
       when is_struct(client, EdgePaymentClient) and is_map(attributes) do
+    client
+    |> EdgePaymentClient.post("#{@path}", %{data: %{type: @resource_type, attributes: attributes}})
+    |> case do
+      {:ok,
+       %{
+         json: %{
+           "data" => entity
+         }
+       }} ->
+        struct_from_entity(entity)
+
+      error ->
+        error
+    end
   end
 
   # @spec delete(EdgePaymentClient.t(), t()) :: nil
@@ -66,4 +80,20 @@ defmodule EdgePaymentClient.Customer do
   #     when is_struct(client, EdgePaymentClient) and is_struct(record, __MODULE__) and
   #            is_map(attributes) do
   # end
+
+  defp struct_from_entity(
+         %{
+           "id" => id,
+           "attributes" => %{"name" => name, "created_at" => created_at, "updated_at" => updated_at}
+         } = record
+       ) do
+    %__MODULE__{
+      id: id,
+      name: name,
+      # TODO: Parse date time
+      created_at: created_at,
+      updated_at: updated_at,
+      __record__: record
+    }
+  end
 end
